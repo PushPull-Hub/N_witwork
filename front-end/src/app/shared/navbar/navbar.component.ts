257f3a88bdf0b8e786a7a1f5dd2918ec6a5de826
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2021-01-18 18:01:15 +0100

    srtyle the  navbar

--- front-end/src/app/shared/navbar/navbar.component.ts
+++ front-end/src/app/shared/navbar/navbar.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  logo: string;
+  constructor() {}
 
   ngOnInit(): void {
+    this.logo = environment.logo;
   }
-
 }
