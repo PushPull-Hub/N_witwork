9e8aedd09c38582e3491b623fd6308c5570cc7ac
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2021-01-18 16:30:48 +0100

    generate navigation component

--- front-end/src/app/components/authenticated/homepage/homepage.module.ts
+++ front-end/src/app/components/authenticated/homepage/homepage.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomepageRoutingModule } from './homepage-routing.module';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NavigationComponent],
   imports: [
     CommonModule,
     HomepageRoutingModule
