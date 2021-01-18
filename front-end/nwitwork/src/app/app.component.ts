62a12fb7ba503ef380a77c840f8ad98e446f16a5
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2021-01-18 11:42:50 +0100

    log on the console the author's name

--- front-end/nwitwork/src/app/app.component.ts
+++ front-end/nwitwork/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'nwitwork';
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+    console.log(
+      '  _    _                                   \n' +
+        ' | |  | |                                   \n' +
+        ' | |__| |   __ _   _ __ ___    ____   __ _  \n' +
+        " |  __  |  / _` | | '_ ` _ \\  |_  /  / _`  |\n" +
+        ' | |  | | | (_| | | | | | | |  / /  | (_| | \n' +
+        ' |_|  |_|  \\__,_| |_| |_| |_| /___|  \\__,_ |'
+    );
+  }
 }
