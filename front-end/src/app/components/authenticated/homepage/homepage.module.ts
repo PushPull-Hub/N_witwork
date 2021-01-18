023175bd5141a9e21d853568b70c708ee687d938
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2021-01-18 17:49:06 +0100

    import navbar component from shared module for a global use

--- front-end/src/app/components/authenticated/homepage/homepage.module.ts
+++ front-end/src/app/components/authenticated/homepage/homepage.module.ts
@@ -4,13 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomepageRoutingModule } from './homepage-routing.module';
 import { NavigationComponent } from './navigation/navigation.component';
 import { MainComponent } from './main/main.component';
-
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [NavigationComponent, MainComponent],
-  imports: [
-    CommonModule,
-    HomepageRoutingModule
-  ]
+  imports: [CommonModule, HomepageRoutingModule, SharedModule],
 })
-export class HomepageModule { }
+export class HomepageModule {}
--- front-end/src/app/shared/shared.module.ts
+++ front-end/src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SpinnerComponent } from './spinner/spinner.component';
+
 import { NavbarComponent } from './navbar/navbar.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 import { PostComponent } from './post/post.component';
 import { CreatePostComponent } from './create-post/create-post.component';
 
 @NgModule({
-  declarations: [SpinnerComponent, NavbarComponent, PostComponent, CreatePostComponent],
+  declarations: [
+    SpinnerComponent,
+    NavbarComponent,
+    PostComponent,
+    CreatePostComponent,
+  ],
   imports: [CommonModule],
+  exports: [
+    SpinnerComponent,
+    NavbarComponent,
+    PostComponent,
+    CreatePostComponent,
+  ],
 })
 export class SharedModule {}
