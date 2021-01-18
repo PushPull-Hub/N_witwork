f30ba1be11c9af9e9b0ff89b2d5557266bff0f89
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2021-01-18 15:36:55 +0100

    generate the initial important components for unauthenticated module

---
+++ front-end/src/app/components/unauthenticated/project-founder/project-founder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-project-founder',
+  templateUrl: './project-founder.component.html',
+  styleUrls: ['./project-founder.component.scss']
+})
+export class ProjectFounderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
