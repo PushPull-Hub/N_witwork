410ad441ab9e75c7789b46ae1562a9225ccd111a
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2021-01-18 15:48:19 +0100

    generate the initial important components for authenticated module

---
+++ front-end/src/app/components/authenticated/relation-ships/relation-ships.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RelationShipsRoutingModule } from './relation-ships-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RelationShipsRoutingModule
+  ]
+})
+export class RelationShipsModule { }
