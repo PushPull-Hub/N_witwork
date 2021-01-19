7b0754890efdb72a5ae0136dccec2c3003ee5423
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2021-01-19 17:58:18 +0100

    save the icons folder path in env file

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AuthenticatedModule } from './components/authenticated/authenticated.module';
 
 @NgModule({
