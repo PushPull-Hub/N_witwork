a8b5e0c58429454e43bb88074df64fe5d45b0fe8
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2021-01-19 10:13:39 +0100

    add the logo path to the env file for global use

--- front-end/src/environments/environment.prod.ts
+++ front-end/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   male_avatar_photo_url: './assets/images/male-user.jpg',
   female_avatar_photo_url: './assets/images/female-user.jpg',
-  logo: './assets/images/scai-logo.png',
+  logo: './assets/images/scai-mini-logo.png',
 };
