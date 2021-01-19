bf9f2d2ab8d28180ffc7420afe57c3d011dbe6cb
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2021-01-19 17:59:35 +0100

    pass the authenticated user  trough input to navbar component

--- front-end/src/app/shared/navbar/navbar.component.ts
+++ front-end/src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User.model';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -7,10 +8,31 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  @Input() authenticatedUser: User;
   navBarLogo: string;
+  profilePictureUrl: string;
+
+  homeIcon: string;
+  messengerIcon: string;
+  friendsIcon: string;
+  notificationIcon: string;
+
   constructor() {}
 
   ngOnInit(): void {
     this.navBarLogo = environment.logo;
+    this.homeIcon = environment.iconsFolderPath + '/home.svg';
+    this.messengerIcon = environment.iconsFolderPath + '/messenger.svg';
+    this.friendsIcon = environment.iconsFolderPath + '/users.svg';
+    this.notificationIcon = environment.iconsFolderPath + '/notification.svg';
+    this.loadProfilePicture();
+  }
+
+  private loadProfilePicture() {
+    this.authenticatedUser.profilePictureUrl
+      ? (this.profilePictureUrl = this.authenticatedUser.profilePictureUrl)
+      : this.authenticatedUser.gender.toString() == 'male'
+      ? (this.profilePictureUrl = environment.male_avatar_photo_url)
+      : (this.profilePictureUrl = environment.female_avatar_photo_url);
   }
 }
