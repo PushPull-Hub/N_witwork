e511ae8f87d243d4c27a3c1cda987c9b8d2a7a87
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2021-02-03 12:56:34 +0100

    create main page for the application

--- front-end/src/app/components/unauthenticated/unauthenticated-wrapper.component.ts
+++ front-end/src/app/components/unauthenticated/unauthenticated-wrapper.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-unauthenticated-wrapper',
   templateUrl: './unauthenticated-wrapper.component.html',
-  styleUrls: ['./unauthenticated-wrapper.component.scss']
+  styleUrls: ['./unauthenticated-wrapper.component.scss'],
 })
 export class UnauthenticatedWrapperComponent implements OnInit {
+  scaiConsultingLogo;
+  linkedInIcon: string;
+  gitHubIcon: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.scaiConsultingLogo = environment.logo;
+    this.linkedInIcon = environment.iconsFolderPath + '/linkedin.svg';
+    this.gitHubIcon = environment.iconsFolderPath + '/github.svg';
   }
-
 }
