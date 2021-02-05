7b973bd057c94dbb6182676c2301358c7194dc1e
Hamza Salvagno <dahhani.hamza1998@gmail.com>
2021-02-05 18:06:44 +0100

    create a welcome page component

--- front-end/src/app/components/unauthenticated/welcome-page/welcome-page.component.ts
+++ front-end/src/app/components/unauthenticated/welcome-page/welcome-page.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-welcome-page',
   templateUrl: './welcome-page.component.html',
-  styleUrls: ['./welcome-page.component.scss']
+  styleUrls: ['./welcome-page.component.scss'],
 })
 export class WelcomePageComponent implements OnInit {
+  scaiConsultingLogo;
+  linkedInIcon: string;
+  gitHubIcon: string;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.scaiConsultingLogo = environment.logo;
+    this.linkedInIcon = environment.iconsFolderPath + '/linkedin.svg';
+    this.gitHubIcon = environment.iconsFolderPath + '/github.svg';
   }
-
 }
