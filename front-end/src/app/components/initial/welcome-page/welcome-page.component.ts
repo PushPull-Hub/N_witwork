586e715ecbe774ed0fb7c5e56f34c36c5d703f7f
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-02-09 17:05:24 +0100

    reset to the previous commit

--- front-end/src/app/components/initial/welcome-page/welcome-page.component.ts
+++ front-end/src/app/components/initial/welcome-page/welcome-page.component.ts
@@ -8,18 +8,9 @@ import { environment } from 'src/environments/environment';
 })
 export class WelcomePageComponent implements OnInit {
   scaiConsultingLogo;
-  linkedInIcon: string;
-  gitHubIcon: string;
-  linkedInLink: string;
-  gitHubLink: string;
-
   constructor() {}
 
   ngOnInit(): void {
     this.scaiConsultingLogo = environment.logo;
-    this.linkedInIcon = environment.iconsFolderPath + '/linkedin.svg';
-    this.gitHubIcon = environment.iconsFolderPath + '/github.svg';
-    this.gitHubLink = 'https://github.com/PushPull-Hub/Nwitwork';
-    this.linkedInLink = 'https://www.linkedin.com/in/hamza-dahhani-a631141b6/';
   }
 }
