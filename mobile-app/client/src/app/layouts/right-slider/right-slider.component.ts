bfd76de6e94e56b5e0d68c34c83e26cdc213761d
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-06-08 17:05:49 +0200

    create the third layer component - ionic menu

---
+++ mobile-app/client/src/app/layouts/right-slider/right-slider.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-right-slider',
+  templateUrl: './right-slider.component.html',
+  styleUrls: ['./right-slider.component.scss'],
+})
+export class RightSliderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
