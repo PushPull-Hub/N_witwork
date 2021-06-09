96518ad9b280d75b3c9facddf0143821ef028f0f
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-06-09 17:30:49 +0200

    add methods to handle error and to execute on server listening

--- mobile-app/server/server.js
+++ mobile-app/server/server.js
@@ -1,7 +1,42 @@
 const app = require('./app');
 const http = require('http');
+const debug = require('debug')('node-angular');
 
-const port = process.env.PORT || "3000";
-const server = http.createServer(app)
+const normalizePort = (value) => {
+  const port = parseInt(value, 10);
+  return isNaN(port) ? value : port >= 0 ? port : false;
+};
 
-server.listen(port)
+const onError = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+  const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + port;
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+const onListening = () => {
+  const adress = server.address();
+  const bind = typeof adress === 'string' ? 'pipe ' + adress : 'port ' + port;
+  debug('Listening on ' + bind);
+};
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+const server = http.createServer(app);
+server.on('listening', onListening);
+server.on('error', onError);
+
+server.listen(port);
