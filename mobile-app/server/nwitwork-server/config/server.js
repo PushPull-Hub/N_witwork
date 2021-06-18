131237ae5a86d4115fa28bdb8c7911520afff51b
Hamza Dahhani <dahhani.hamza1998@gmail.com>
2021-06-18 16:56:02 +0200

    the initial commit - creating the project

---
+++ mobile-app/server/nwitwork-server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '77cb8ff4f890abbac86033fc044a839e'),
+    },
+  },
+});
---
+++ mobile-app/server/nwitwork-server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '28d1bfb0-ce42-4e43-9fc7-d1f812a4be3c'
+};
